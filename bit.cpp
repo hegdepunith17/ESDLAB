0261506e203e30ca124ead29fc43f2a41500eae2
Punith B Hegde <58084673+hegdepunith17@users.noreply.github.com>
2022-05-03 19:12:57 +0530

    Create bit.cpp

---
+++ bit.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int
+main ()
+{
+  int a = 7;
+  int b = 3;
+printf ("\n The value of a AND b: %d \n", a & b);
+printf ("\n The value of a OR b: %d \n", a | b);
+printf ("\n The value of a XOR b: %d \n", a ^ b);
+printf ("\n The value of ~a: %d \n", ~a);
+printf ("\n The value of a>>1: %d \n", a >> 1);
+printf ("\n The value of a>>2: %d \n", a >> 2);
+printf ("\n The value of b<<1: %d \n", b << 1);
+printf ("\n The value of b<<2: %d \n", b << 2);
+}
